96622a6a62fd7646260b0bebc5c06d24e00dfeec
Shakti Suraj <shaktisuraj35@gmail.com>
2025-10-26 22:03:23 +0530

    code update

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(helmet());
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'https://improvemycity-two.vercel.app/',
   credentials: true
 }));
 
